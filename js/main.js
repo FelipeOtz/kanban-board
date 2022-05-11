6bc88a7fceac7e23a32141db613c86c7bc917e47
FelipeOtz <felipeht25@gmail.com>
2022-05-11 16:57:18 -0300

    fazendo excluir

--- js/main.js
+++ js/main.js
@@ -1,23 +1,37 @@
 const addButton = document.querySelectorAll(".add-item");
 const column = document.querySelectorAll(".items");
-const icon = document.querySelectorAll(".icon");
+const icons = document.querySelectorAll(".icon");
+let cards = document.querySelectorAll(".card");
+const board = document.querySelector(".board");
 
 addButton.forEach((btn, index) => {
   btn.addEventListener("click", () => {
-    const card = document.createElement("input");
-    card.classList.add("item-input");
-    card.setAttribute("contenteditable", "");
+    const card = document.createElement("div");
+    const cardInput = document.createElement("input");
+    cardInput.classList.add("card-input");
+    cardInput.setAttribute("Type", "text");
+    card.classList.add("card");
+    card.setAttribute("draggable", "true");
+    card.appendChild(cardInput);
     column[index].appendChild(card);
+    cards = document.querySelectorAll(".card");
   });
 });
 
-addButton.forEach((btn, index) => {
-  btn.addEventListener("dblclick", () => {
-    column[index].removeChild;
+cards.forEach((card, index) => {
+  card.addEventListener("click", () => {
+    cards = document.querySelectorAll(".card");
+    console.log(index);
+    column[0].removeChild(column[0].children[index]);
   });
 });
 
-function changeIcons(colIndex, icon) {
-  if (colIndex === 1) {
-  }
+cards.forEach((card, index) => {
+  dragndrop(card);
+});
+
+function dragndrop(card) {
+  card.addEventListener("dragstart", () => {
+    console.log("oi");
+  });
 }
