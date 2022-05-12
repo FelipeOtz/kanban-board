f622db56f2a4bdc2236d863cc6e017caf417a235
Felipe <felipeht25@gmail.com>
2022-05-12 01:10:52 -0300

    finalizando icone e delete completo

--- js/main.js
+++ js/main.js
@@ -2,27 +2,38 @@ const addButton = document.querySelectorAll(".add-item");
 const column = document.querySelectorAll(".items");
 const icons = document.querySelectorAll(".icon");
 let cards = document.querySelectorAll(".card");
+
 const board = document.querySelector(".board");
+
+const cardIcon = document.querySelectorAll("card-icon");
 
 addButton.forEach((btn, index) => {
   btn.addEventListener("click", () => {
     const card = document.createElement("div");
     const cardInput = document.createElement("input");
+    const cardIcon = document.createElement("div");
+    const imgIcon = document.createElement("img");
+
     cardInput.classList.add("card-input");
     cardInput.setAttribute("Type", "text");
+    cardIcon.classList.add("card-icon");
+    imgIcon.src = "icons/trash.png";
+    cardIcon.appendChild(imgIcon);
     card.classList.add("card");
     card.setAttribute("draggable", "true");
     card.appendChild(cardInput);
+    card.appendChild(cardIcon);
     column[index].appendChild(card);
-    cards = document.querySelectorAll(".card");
-  });
-});
 
-cards.forEach((card, index) => {
-  card.addEventListener("click", () => {
     cards = document.querySelectorAll(".card");
-    console.log(index);
-    column[0].removeChild(column[0].children[index]);
+
+    cards.forEach((card, index) => {
+      card.addEventListener("click", (e) => {
+        if (e.target.classList.contains("card-icon")) {
+          card.remove();
+        }
+      });
+    });
   });
 });
 
