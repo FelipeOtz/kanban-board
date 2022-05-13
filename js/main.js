b1313d9031a895540fe311c04c2aadbe9b724172
FelipeOtz <felipeht25@gmail.com>
2022-05-13 03:27:58 -0300

    Estilização completa e delete com animção

--- js/main.js
+++ js/main.js
@@ -1,48 +1,70 @@
 const addButton = document.querySelectorAll(".add-item");
-const column = document.querySelectorAll(".items");
+const columns = document.querySelectorAll(".items");
 const icons = document.querySelectorAll(".icon");
 let cards = document.querySelectorAll(".card");
-
 const board = document.querySelector(".board");
-
 const cardIcon = document.querySelectorAll("card-icon");
+
+function addCard(index) {
+  const card = document.createElement("div");
+  const cardInput = document.createElement("input");
+  const cardIcon = document.createElement("div");
+  const imgIcon = document.createElement("img");
+
+  cardInput.classList.add("card-input");
+  cardInput.setAttribute("Type", "text");
+  cardIcon.classList.add("card-icon");
+  imgIcon.src = "icons/trash.png";
+  cardIcon.appendChild(imgIcon);
+  card.classList.add("card");
+  card.setAttribute("draggable", "true");
+  card.appendChild(cardInput);
+  card.appendChild(cardIcon);
+  columns[index].appendChild(card);
+  cards = document.querySelectorAll(".card");
+  handlecard();
+}
 
 addButton.forEach((btn, index) => {
   btn.addEventListener("click", () => {
-    const card = document.createElement("div");
-    const cardInput = document.createElement("input");
-    const cardIcon = document.createElement("div");
-    const imgIcon = document.createElement("img");
+    addCard(index);
+  });
+});
 
-    cardInput.classList.add("card-input");
-    cardInput.setAttribute("Type", "text");
-    cardIcon.classList.add("card-icon");
-    imgIcon.src = "icons/trash.png";
-    cardIcon.appendChild(imgIcon);
-    card.classList.add("card");
-    card.setAttribute("draggable", "true");
-    card.appendChild(cardInput);
-    card.appendChild(cardIcon);
-    column[index].appendChild(card);
+function handlecard() {
+  cards.forEach((card, index) => {
+    card.addEventListener("click", (e) => {
+      if (e.target.classList.contains("card-icon")) {
+        card.remove();
+      }
+    });
 
-    cards = document.querySelectorAll(".card");
+    card.addEventListener("dragstart", (e) => {
+      console.log(card);
+      card.classList.add("dragging");
+    });
 
-    cards.forEach((card, index) => {
-      card.addEventListener("click", (e) => {
-        if (e.target.classList.contains("card-icon")) {
-          card.remove();
-        }
-      });
+    card.addEventListener("dragend", (e) => {
+      console.log(card);
+      card.classList.remove("dragging");
+    });
+
+    card.addEventListener("mouseover", async (e) => {
+      const icon = card.querySelector(".card-icon");
+      icon.classList.add("active");
+      setTimeout(() => {
+        icon.classList.remove("active");
+      }, 2000);
     });
   });
-});
-
-cards.forEach((card, index) => {
-  dragndrop(card);
-});
-
-function dragndrop(card) {
-  card.addEventListener("dragstart", () => {
-    console.log("oi");
-  });
 }
+
+columns.forEach((column) => {
+  column.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    const draggingElement = document.querySelector(".dragging");
+    column.appendChild(draggingElement);
+  });
+});
+
+handlecard();
